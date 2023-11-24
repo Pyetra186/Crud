09004438613af23df757a1eb27731489709fc4f5
PIETRA PAULA VOLPATO <23111825@CFP127ALUNO.LOCAL>
2023-11-24 14:50:47 -0300

    proxima-aula

---
+++ usuarios.js
@@ -0,0 +1,66 @@
+'use strict'
+
+async function listarUsuarios () {
+    const url ='http://10.107.140.2:8080/usuarios/'
+    const response = await fetch(url)
+    const data = await response.json ()
+    console.log(data)
+    return data
+}
+
+async function cadastrarUsuario (usuario) {
+    const url ='http://10.107.140.2:8080/usuarios/'
+    const options = {
+        method: 'POST',
+        headers : {
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify(usuario)
+    }
+    const response = await fetch(url, options)
+    console.log (response.ok)
+    return response.ok
+}
+
+async function atualizarUsuario (usuario){
+    const url = `http://10.107.140.2:8080/usuarios/${usuario.id}`
+    const options = {
+        method: 'PUT',
+        headers : {
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify(usuario)
+    }
+    const response = await fetch(url, options)
+    console.log (response.ok)
+    return response.ok
+}
+
+async function deletarUsuario (id) {
+    const url = `http://10.107.140.2:8080/usuarios/${id}`
+    const options = {
+        method: 'DELETE'
+    }
+    const response = await fetch(url, options)
+    console.log (response.ok)
+    return response.ok
+}
+
+const usuarioNovo = {
+    nome: 'Pietra',
+    email: 'pietra@gmail.com'
+}
+
+deletarUsuario(125)
+
+//const usuarioAtualizado = {
+//   id: '125',
+//    nome: 'Roberta',
+//    email: 'roberta@gmail.com'
+//}
+
+//atualizarUsuario (usuarioAtualizado)
+
+//cadastrarUsuario(usuarioNovo)
+
+//listarUsuarios()
